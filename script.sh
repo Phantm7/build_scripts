55ed1b7eed26aa33d31709909a80b0708c083379
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-07-21 23:23:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,6 +30,6 @@ echo "======= Export Done ======"
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-lineage_Mi439_4_19-ap2a-userdebug
+lunch lineage_Mi439_4_19-ap2a-userdebug
 make installclean
 mka bacon
