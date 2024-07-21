6809373fb360fc95812aa4c0093740a12c9562aa
PhantomEnigma <1212ravi@proton.me>
2024-07-21 19:56:06 +0530

    Initial

--- script.sh
+++ script.sh
@@ -1,11 +1,35 @@
+#!/bin/bash
+
 rm -rf .repo/local_manifests/
-#repo init rom
+
 repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests .repo/local_manifests -b a14
-#build
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14-evoxyz .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
 /opt/crave/resync.sh
-. build/envsetup.sh
-lunch lineage_Mi439-ap1a-userdebug
+echo "============="
+echo "Sync success"
+echo "============="
+
+
+# Export
+export BUILD_USERNAME=Phantom
+export BUILD_HOSTNAME=crave
+export MITHORIUM_QCOM_HALS_DEFAULT_VARIANT=LA.UM.9.6.4.r1-05500-89xx.QSSI13.0
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+lineage_Mi439_4_19-ap2a-userdebug
 make installclean
-brunch Mi439_4_19 || brunch lineage_Mi439_4_19-ap1a-userdebug
+mka bacon
