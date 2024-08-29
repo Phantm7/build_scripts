09ab59e9fc2821e3aba931d55436e97b1e36c2ca
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-08-29 14:03:29 +0100

    init

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@
 
 rm -rf .repo/local_manifests/
 
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14-crd10 .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a13-rev .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -22,19 +22,14 @@ echo "============="
 # Export
 export BUILD_USERNAME=Phantom
 export BUILD_HOSTNAME=crave
-export MITHORIUM_QCOM_HALS_DEFAULT_VARIANT=LA.UM.9.6.4.r1-05500-89xx.QSSI13.0
+#export MITHORIUM_QCOM_HALS_DEFAULT_VARIANT=LA.UM.9.6.4.r1-05500-89xx.QSSI13.0
 echo "======= Export Done ======"
 
-#Cherry-pick
-cd vendor/addons
-git fetch crdroid --unshallow
-git fetch https://github.com/RisingTechOSS/android_vendor_addons fourteen
-git cherry-pick dbd659e
-cd ../..
+
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-lunch lineage_Mi439_4_19-ap2a-userdebug
+lunch lineage_Mi439_4_19-userdebug
 make installclean
 mka bacon
