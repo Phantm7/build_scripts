59028fbb23aab9f61e18d2d04b6a2f02b4c7b3ee
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-09-15 11:56:15 +0100

    Remove some extra crap :(

--- script.sh
+++ script.sh
@@ -36,44 +36,20 @@ git fetch https://github.com/PhantomEnigma/android_packages_apps_Updater 13.0
 git cherry-pick 946090a
 cd ../../..
 
-#App Downgrade
-cd frameworks/base
-git fetch crdroid --unshallow
-git fetch https://github.com/RisingTechOSS/android_frameworks_base fourteen
-git cherry-pick ba93896
-cd ../..
-
 #Bypass SAF
 cd frameworks/base
 git fetch crdroid --unshallow
 git fetch https://github.com/PhantomEnigma/android_frameworks_base 13.0
 git cherry-pick 7b68921
-
 cd ../..
 
 echo "===== Cherry-pick Ended ====="
 # Export
 export BUILD_USERNAME=Phantom
 export BUILD_HOSTNAME=crave
-#export MITHORIUM_QCOM_HALS_DEFAULT_VARIANT=LA.UM.9.6.4.r1-05500-89xx.QSSI13.0
 echo "======= Export Done ======"
 
 
-# Set up build environment
-source build/envsetup.sh
-echo "====== Envsetup Done ======="
-
-lunch lineage_Mi439_4_19-userdebug
-make installclean
-mka bacon
-
-cp out/target/product/*/*.zip .
-echo "Test started"
-cd frameworks/base
-git fetch crdroid --unshallow
-git fetch https://github.com/PhantomEnigma/android_frameworks_base 13.0
-git cherry-pick 00b7ee6
-cd ../..
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
