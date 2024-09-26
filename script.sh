069dc43e79323a24730098fb97446a2fc37b0b2e
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-09-26 03:14:38 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@
 
 rm -rf .repo/local_manifests/
 
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14-crd .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14-afterlife .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -20,34 +20,12 @@ echo "Sync success"
 echo "============="
 
 # Export
-export BUILD_USERNAME=Phantom
-export BUILD_HOSTNAME=crave
+#export BUILD_USERNAME=Phantom
+#export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
-#Cherry-pick
-cd vendor/addons
-git fetch crdroid --unshallow
-git fetch https://github.com/RisingTechOSS/android_vendor_addons fourteen
-git cherry-pick dbd659e
-
-#Framework_base patches
-cd frameworks/base
-git fetch crdroid --unshallow
-git fetch https://github.com/RisingTechOSS/android_frameworks_base fourteen
-#Force LTA CA
-git cherry-pick 9756c4c
-#Downgrade
-git cherry-pick ba93896
-#SAF
-git fetch https://github.com/PhantomEnigma/android_frameworks_base 14.0
-git cherry-pick 7903db5
-git cherry-pick be9a03b
-git cherry-pick 1eb9c08
-cd ../..
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-lunch lineage_Mi439_4_19-ap2a-userdebug
-make installclean
-mka bacon
+goafterlife Mi439_4_19 || goafterlife Mi439_4_19-userdebug || launch afterlife-ap3a
